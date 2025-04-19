6fb5883eb604271298081eec3cde12781ed458d1
Th1agoFarias <th.silva.farias@gmail.com>
2025-04-19 11:55:03 -0300

    Salvando alterações locais antes de rebase

--- src/model_train.py
+++ src/model_train.py
@@ -1,10 +1,20 @@
-import pandas  as pd
-from sqlalchemy import  create_engine
+import pandas as pd
+from sqlalchemy import create_engine, text
 
-def run_etl(db_path='data/oulad.db'):
-    engine = create_engine(f"sqlite:///{db_path}")
 
-    query = ""
+def run_features(db_path='data/oulad.db', query_path='src/features_student.sql'):
 
-    with engine.begin() as conn:
-        conn.execute(query)
+    with open(query_path, 'r') as f:
+      query = f.read()
+
+      engine = create_engine(f'sqlite:///{db_path}')
+      with engine.connect() as conn:
+        df = pd.read_sql_query(query, conn)
+
+    return df
+    
+    
+
+
+
+        
