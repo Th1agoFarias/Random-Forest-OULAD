2b4614c81b507a506cc3811f7a40465bce72f343
Thiago Farias <108031064+Th1agoFarias@users.noreply.github.com>
2026-09-10 16:22:37 -0300

    Fix ingestion function and duplicate table write

--- src/ingestion.py
+++ src/ingestion.py
@@ -1,17 +1,20 @@
-import pandas as pd 
-import json 
+import json
+
+import pandas as pd
 from sqlalchemy import create_engine
+
 from utils import get_logger
 
 logger = get_logger(__name__)
 
-def run_igestion(json_path="ingestion.json", db_path="data/oulad.db"):
-    engine = create_engine(f"sqlite:///{db_path}")
-    with open(json_path, "r") as f:
-        tables = json.load(f)
-        for table in tables:
-            df = pd.read_csv(table["path"])
-            df.to_sql(table['path'], con=engine, if_exists="replace", index=False)
-            df.to_sql(table['table'], engine, if_exists="replace", index=False)
-            logger.info(f"Table {table['table']} ingestão completa.")
 
+def run_ingestion(json_path="ingestion.json", db_path="data/oulad.db"):
+    engine = create_engine(f"sqlite:///{db_path}")
+
+    with open(json_path, "r", encoding="utf-8") as f:
+        tables = json.load(f)
+
+    for table in tables:
+        df = pd.read_csv(table["path"])
+        df.to_sql(table["table"], con=engine, if_exists="replace", index=False)
+        logger.info(f"Tabela {table['table']} ingerida com sucesso.")
